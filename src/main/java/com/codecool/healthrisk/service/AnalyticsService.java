d1c54c813b1192000bddd97670a057f4216f0061
szigetvarikati <szigetvarikati@gmail.com>
2023-04-21 11:13:20 +0200

    implement Age calculation and it passed 2 tests

--- src/main/java/com/codecool/healthrisk/service/AnalyticsService.java
+++ src/main/java/com/codecool/healthrisk/service/AnalyticsService.java
@@ -3,13 +3,29 @@ package com.codecool.healthrisk.service;
 import com.codecool.healthrisk.data.Person;
 import com.codecool.healthrisk.data.WeightCondition;
 
+import java.time.LocalDate;
+
 public class AnalyticsService {
 
     public static final double OVERWEIGHT_BMI_LIMIT = 25.0;
     public static final int WEIGHT_CONDITION_YEARS_LIMIT = 3;
 
     public int calculateAge(Person person) {
-        return 0;
+        String birthDateStr = person.getBirthDate();
+
+        String[] parts = birthDateStr.split("/");
+        int birthDay = Integer.parseInt(parts[0]);
+        int birthMonth = Integer.parseInt(parts[1]);
+        int birthYear = Integer.parseInt(parts[2]);
+
+        LocalDate currentDate = LocalDate.now();
+
+        int age = currentDate.getYear() - birthYear;
+        if (currentDate.getMonthValue() < birthMonth ||
+                (currentDate.getMonthValue() == birthMonth && currentDate.getDayOfMonth() < birthDay)) {
+           age--;
+        }
+        return age;
     }
 
     public double[] calculateBMISeries(Person person) {
